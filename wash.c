707af21ab26d917270243a44f6abc523dad3e0fb
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2024-06-11 14:20:37 -0700

    Wow this is getting tricky

--- wash.c
+++ wash.c
@@ -5,15 +5,59 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <string.h>
+
 
 #define MAX_LINE 256
 
+
+
 void parse_input(char *input, char **args, int *background) {
-    // Implementation of input parsing
+    *background = 0;
+
+    // Trim leading and trailing whitespace
+    char *end;
+    while (isspace((unsigned char)*input)) input++;
+    if (*input == 0) return; // All spaces
+
+    end = input + strlen(input) - 1;
+    while (end > input && isspace((unsigned char)*end)) end--;
+    end[1] = '\0';
+
+    // Tokenize the input string
+    int i = 0;
+    char *token = strtok(input, " ");
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    // Check if the command should be run in the background
+    if (i > 0 && strcmp(args[i-1], "&") == 0) {
+        *background = 1;
+        args[i-1] = NULL; // Remove '&' from args
+    }
 }
 
 void execute_command(char **args) {
-    // Implementation of command execution using fork and exec
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        // Error forking
+        perror("fork");
+        return;
+    } else if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("execvp");
+        }
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process
+        int status;
+        waitpid(pid, &status, 0);
+    }
 }
 
 int main() {
@@ -23,23 +67,45 @@ int main() {
 
     while (1) {
         printf("wash> ");
-        fgets(input, MAX_LINE, stdin);
+        if (fgets(input, MAX_LINE, stdin) == NULL) {
+            // Handle end of input (Ctrl+D)
+            printf("\n");
+            break;
+        }
+
+        // Remove newline character
+        input[strcspn(input, "\n")] = '\0';
 
         parse_input(input, args, &background);
 
         // Check for built-in commands and handle them
+        if (args[0] == NULL) continue; // Empty input
+
         if (strcmp(args[0], "exit") == 0) {
             break;
         } else if (strcmp(args[0], "echo") == 0) {
-            // Handle echo
+            for (int i = 1; args[i] != NULL; i++) {
+                printf("%s ", args[i]);
+            }
+            printf("\n");
         } else if (strcmp(args[0], "pwd") == 0) {
-            // Handle pwd
+            char cwd[MAX_LINE];
+            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                printf("%s\n", cwd);
+            } else {
+                perror("getcwd");
+            }
         } else if (strcmp(args[0], "cd") == 0) {
-            // Handle cd
+            if (args[1] == NULL) {
+                args[1] = getenv("HOME");
+            }
+            if (chdir(args[1]) != 0) {
+                perror("cd");
+            }
         } else if (strcmp(args[0], "setpath") == 0) {
-            // Handle setpath
+            // Implementation of setpath (not included in this snippet)
         } else if (strcmp(args[0], "help") == 0) {
-            // Handle help
+            // Implementation of help (not included in this snippet)
         } else {
             execute_command(args);
         }
@@ -50,4 +116,3 @@ int main() {
     }
     return 0;
 }
-
